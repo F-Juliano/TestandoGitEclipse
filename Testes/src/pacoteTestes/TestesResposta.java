86419f989a55da404f70d015f1142d06ffb6be16
F-Juliano <3juliano3@globomail.com>
2016-08-24 20:24:20 -0300

    Onde aparece esse Update teste resposta
    Onde aparece esse Add an optional

--- Testes/src/pacoteTestes/TestesResposta.java
+++ Testes/src/pacoteTestes/TestesResposta.java
@@ -3,7 +3,7 @@ package pacoteTestes;
 public class TestesResposta {
 	static void soma(int a, int b) {
 		int s = a + b;
-		System.out.println("A soma dos dois valores È: " + s);
+		System.out.println("A soma dos dois valores √©: " + s);
 	}
 
 	static int somaReturn(int a, int b) {
@@ -13,23 +13,24 @@ public class TestesResposta {
 	}
 	
 	static int calcularReturn(int a, int b, int c){
-		return somaReturn(a, b)*c; //usando o mÈtodo somaReturn (q retorna um inteiro) no c·lculo
+		return somaReturn(a, b)*c; //usando o m√©todo somaReturn (q retorna um inteiro) no c√°lculo
 	}
 	
 //	static int calcularVoid(int a, int b, int c){
-//		return soma(a, b)*c; //tentando usar o mÈtodo soma (q retorna void) no c·lculo - gera erro de compilaÁ„o
+//		return soma(a, b)*c; //tentando usar o m√©todo soma (q retorna void) no c√°lculo - gera erro de compila√ß√£o
 //	}
 
 	public static void main(String[] args) {
 
-		System.out.println("MÈtodo void");
+		System.out.println("M√©todo void");
 		soma(5, 2);
 
 		int sm = somaReturn(5, 2);
-		System.out.println("MÈtodo com retorno");
-		System.out.println("A soma dos valores È: " + sm);
+		System.out.println("M√©todo com retorno");
+		System.out.println("A soma dos valores √©: " + sm);
 		
-		System.out.println("Resultado do c·lculo: " + calcularReturn(10, 5, 3));
+		System.out.println("Resultado do c√°lculo: " + calcularReturn(10, 5, 3));
 	}
+	//coment√°rio apenas para testar
 
 }
